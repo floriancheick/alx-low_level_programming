a9302e8bcb93491c3420ace089e6b8945430dc55
root <f.nganji@alustudent.com>
2022-09-12 06:27:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
-  printf("with proper grammar, but the outcome is piece of art");
+/**
+ *main - print the string
+ *
+ *Decription: using the main function
+ *this program prints "Programming is like building a multilingual puzzle
+ *Return: 0
+ */
+int main(void) {
+  printf("with proper grammar, but the outcome is piece of art,\n");
   return 0;
 }
