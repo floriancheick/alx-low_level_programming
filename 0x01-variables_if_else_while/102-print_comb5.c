b318d7fc098af606338446a3449533f059c908b9
root <f.nganji@alustudent.com>
2022-09-12 13:46:35 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point of the program
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+  int i = 0, i2;
+  while (i <= 99)
+    {
+      i2 = i;
+      while (i2 <= 99)
+	{
+	  if (i2 != i)
+	    {
+	      putchar((i / 10) + 48);
+	      putchar((i % 10) + 48);
+	      putchar(' ');
+	      putchar((i2 / 10) + 48);
+	      putchar((i2 % 10) + 48);
+	      if (i != 98 || i2 != 99)
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+	    }
+	  ++i2;
+	}
+      ++i;
+    }
+  putchar('\n');
+  return (0);
+}
