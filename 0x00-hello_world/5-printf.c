c46e8b5d494e497be1d0dc056a45a0994ae74f83
root <f.nganji@alustudent.com>
2022-09-12 06:29:33 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,8 @@
  *this program prints "Programming is like building a multilingual puzzle
  *Return: 0
  */
-int main(void) {
+int main(void)
+{
   printf("with proper grammar, but the outcome is piece of art,\n");
-  return 0;
+  return (0);
 }
