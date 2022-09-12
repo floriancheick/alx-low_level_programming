4e5ad67f4003061e58fc99253c95ce7bc7deea55
root <f.nganji@alustudent.com>
2022-09-12 06:01:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/*
-This program prints "Programming is like building a multilingual puzzle
- */
+/**
+ *This program prints "Programming is like building a multilingual puzzle
+**/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
