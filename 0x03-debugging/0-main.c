f432393d84c0b9802219ca93fc500214df59f94e
root <f.nganji@alustudent.com>
2022-09-15 16:13:25 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test the function if the integer is negative or positive
+ *
+ *Return 0
+ */
+
+int main(void)
+{
+  int i;
+  i = 0;
+  positive_or_negative(i);
+  return (0);
+}
