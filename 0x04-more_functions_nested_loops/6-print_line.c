e5704dde5ce2e64f846f8b7d1ac03a17334fe5d0
root <f.nganji@alustudent.com>
2022-09-16 10:03:05 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line -  Print a line of n length
+ *@n
+ *Return: nothing
+ */
+void print_line(int n)
+{
+  int c = 0;
+
+  while (c < n)
+    {
+      if (n > 0)
+	_putchar('_');
+      c++;
+    }
+  _putchar('\n');
+}
