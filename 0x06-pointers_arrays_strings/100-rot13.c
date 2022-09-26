4115abd4d4edfa19b08decd364e175b8abcc4813
root <f.nganji@alustudent.com>
2022-09-26 11:00:51 -0700

    first-commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,11 +9,11 @@ char *rot13(char *s)
 {
 int i = 0;
 
-while (s[i] != '\0')
-{
-while ((s[i] >= 'a' && s[i] <= 'z') ||
+if (s[i] != '\0')
+((s[i] >= 'a' && s[i] <= 'z') ||
+else
 (s[i] >= 'A' && s[i] <= 'Z'))
- {
+
 if ((s[i] >= 'a' && s[i] <= 'm') ||
 (s[i] >= 'A' && s[i] <= 'M'))
 s[i] += 13;
