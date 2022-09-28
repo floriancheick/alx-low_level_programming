dd51661ef275ceaa47e781d3f7248e7a47073228
root <f.nganji@alustudent.com>
2022-09-28 16:36:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,16 @@
 *@s: Input string to print
 *Return: Nothing
 */
+
 void _puts_recursion(char *s)
+
 {
-if(*s)
+if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
 _putchar('\n');
-return 0;
+return;
+}
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
