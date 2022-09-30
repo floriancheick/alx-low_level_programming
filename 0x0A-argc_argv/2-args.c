9d28db09160972ca887eabf2147c7bc503bcc0f9
root <f.nganji@alustudent.com>
2022-09-30 08:55:05 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - This function print each argument that passed to the program line
+*@argc: argument counter
+*argv: pointer to array of arguments
+*Return: 0 when it succed
+*/
+int main(int argc, char *argv[])
+{
+
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
