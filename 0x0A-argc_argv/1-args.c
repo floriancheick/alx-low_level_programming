f76eb7152f750ca8013210128f595f5ff6868837
root <f.nganji@alustudent.com>
2022-09-30 08:46:07 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - This function print the number of argument that should be passed in the program
+*@argc: argument counter
+*@argv: Pointer to array of arguments
+*Return: 0 if it succed
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+if (argc > 0)
+printf("%d\n", argc - 1);
+
+return (0);
+}
