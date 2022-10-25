5fbdae5d38744786800ec170721e350c790fb63a
root <f.nganji@alustudent.com>
2022-10-25 09:03:48 -0700

    final_commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,7 +17,11 @@ typedef struct listint_s
   struct listint_s *next;
 } listint_t;
 
-
+typedef struct listp_s
+{
+  void *p;
+  struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
