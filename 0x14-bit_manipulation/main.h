bc4e40dc12ff44212f46da3115bd4ebfc7227b64
root <f.nganji@alustudent.com>
2022-10-29 13:35:00 -0700

    commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,11 +2,11 @@
 #define MAIN_H
 
 /* Prototype */
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
-int set_bit(ubsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-ununsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
